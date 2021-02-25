d858aaa6739e74a8b1ce2ad495ec25a29a5b34bb
patelisii <34968106+patelisii@users.noreply.github.com>
2021-02-25 14:28:17 -0500

    Flix pt 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Patrick Elisii on 2/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
